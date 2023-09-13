41fdc0312e9ba7a7892710e18f587790ae216fe0
Lam giao su <nghiemkhaclam185.mta@gmail.com>
2023-09-13 19:07:14 +0700

    load more

--- src/App.tsx
+++ src/App.tsx
@@ -11,21 +11,38 @@ interface Pokemons {
 
 const App:React.FC = () => {
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+  const [nextUrl, setNextUrl] = useState<string>("");
   useEffect(() => {
     const getPokemon = async() => {
-      const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=20");
+      const res = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
+      setNextUrl(res.data.next);
       res.data.results.forEach(async(pokemon:Pokemons) => {
         const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
         setPokemons((p) => [...p, poke.data]);
       })
     }
     getPokemon();
-  }, [])
+  }, []);
+
+  const loadmore = async () => {
+    let res = await axios.get(nextUrl);
+    setNextUrl(res.data.next);
+    res.data.results.forEach(async (pokemon: Pokemons) => {
+      const poke = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`);
+      setPokemons((p) => [...p, poke.data])
+    })
+  }
+
   return (
     <div className="App">
       <div className="container">
         <header className="pokemon-header">Pokemon</header>
         <PokemonColection pokemons = {pokemons}/>
+        <div className="btn" onClick={loadmore}>
+          <button>
+            Load more
+          </button>
+        </div>
       </div>
     </div>
   );
